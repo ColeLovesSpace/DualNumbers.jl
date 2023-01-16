acf4a61c0b397abd0145e7867168ddbb45bf3980
Cole Coughlin <ccoughlin@perimeterinstitute.ca>
2023-01-16 14:52:32 -0500

    scalar multiplication

--- src/DualNumbers.jl
+++ src/DualNumbers.jl
@@ -10,10 +10,17 @@ struct Dual{T}
     dual::T
 end
 
+# identity and negation ect
+Base.:+(x::Dual) = Dual(+x.real, +x.dual)
 Base.:-(x::Dual) = Dual(-x.real, -x.dual)
 
+# addition and subtraction operatiors
 Base.:+(x::Dual, y::Dual) = Dual(x.real + y.real, x.dual + y.dual)
 # Base.:-(x::Dual, y::Dual) = Dual(x.real - y.real, x.dual - y.dual)
 Base.:-(x::Dual, y::Dual) = x + -y
+
+# scalar multiplication
+Base.:*(a::Number, x::Dual) = Dual(a*x.real, a*x.dual)
+Base.:*(x::Dual, a::Number) = Dual(a*x.real, a*x.dual)
 
 end # module DualNumbers
